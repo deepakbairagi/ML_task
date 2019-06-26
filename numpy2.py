d3b77ab5bc3bb4c31f2df806d3f299784d633464
Deepak Bairagi <51380433+deepakbairagi@users.noreply.github.com>
2019-06-26 23:50:56 +0530

    Add files via upload

---
+++ numpy2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import numpy as np
+#create a numpy array of  8x2 as having number  in each cell between 100 and 200 such that difference between each element is 5 
+print("array of  8x2")
+sArray = np.arange(100, 200, 5)
+print(sArray)
